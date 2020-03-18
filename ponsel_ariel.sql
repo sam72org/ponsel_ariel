195a82ed5c204b4e8f05ed7a937c8e4b9b9889ea
Samuel <muel1410@gmail.com>
2020-03-18 18:47:27 +0700

    upload database

--- ponsel_ariel.sql
+++ ponsel_ariel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 18, 2019 at 06:54 AM
+-- Generation Time: Mar 18, 2020 at 12:46 PM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.0.27
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `db_inventory`
+-- Database: `ponsel_ariel`
 --
 
 -- --------------------------------------------------------
@@ -34,23 +34,6 @@ CREATE TABLE `migrations` (
   `batch` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dumping data for table `migrations`
---
-
-INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2019_10_04_095237_create_ref_pelanggans_table', 1),
-(2, '2019_10_04_195213_create_ref_barangs_table', 2),
-(3, '2019_10_04_195340_create_ref_barangs_table', 3),
-(4, '2019_10_04_200301_create_ref_barangs_table', 4),
-(5, '2019_10_04_201322_create_ta_penjualans_table', 5),
-(6, '2019_10_04_202217_create_ta_detail_penjualans_table', 6),
-(7, '2019_10_05_171737_create_ta_penjualans_table', 7),
-(8, '2019_10_05_195135_create_ta_pembelians_table', 8),
-(9, '2019_10_05_210440_create_ta_pembelians_table', 9),
-(10, '2019_10_05_210540_create_ta_detail_pembelians_table', 10),
-(11, '2019_10_05_212209_create_ref_distributors_table', 11);
-
 -- --------------------------------------------------------
 
 --
@@ -59,10 +42,13 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 
 CREATE TABLE `ref_barang` (
   `id` int(10) NOT NULL,
+  `kode_barcode` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
   `nama_barang` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `kategori_id` tinyint(4) NOT NULL,
   `harga_beli` double NOT NULL,
-  `stok` smallint(6) NOT NULL,
+  `harga_jual` double NOT NULL,
+  `harga_nego` double NOT NULL,
+  `stok` int(11) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -95,13 +81,18 @@ CREATE TABLE `ref_kategori` (
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `ref_kategori`
+-- Table structure for table `ref_operator_pulsa`
 --
 
-INSERT INTO `ref_kategori` (`id`, `kategori`, `created_at`, `updated_at`) VALUES
-(1, 'Case', '2019-10-04 11:39:07', '2019-06-30 04:23:26'),
-(2, 'Handsfree', '2019-10-12 06:45:01', '2019-10-12 06:45:01');
+CREATE TABLE `ref_operator_pulsa` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `operator` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -128,7 +119,7 @@ CREATE TABLE `ta_detail_pembelian` (
   `id` int(10) NOT NULL,
   `no_pembelian` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `barang_id` int(10) NOT NULL,
-  `qty` tinyint(4) NOT NULL,
+  `qty` int(11) NOT NULL,
   `harga` double NOT NULL,
   `sub_total` double NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
@@ -143,9 +134,10 @@ CREATE TABLE `ta_detail_pembelian` (
 
 CREATE TABLE `ta_detail_penjualan` (
   `id` int(10) NOT NULL,
+  `ta_penjualan_id` int(10) NOT NULL,
   `no_penjualan` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `barang_id` int(10) NOT NULL,
-  `qty` tinyint(4) NOT NULL,
+  `qty` int(11) NOT NULL,
   `harga` double NOT NULL,
   `sub_total` double NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
@@ -175,6 +167,24 @@ CREATE TABLE `ta_history_barang` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `ta_jasa`
+--
+
+CREATE TABLE `ta_jasa` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nama` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `no_hp` varchar(12) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `teknisi` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ket` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `harga` int(11) NOT NULL,
+  `user_id` smallint(4) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `ta_pembelian`
 --
 
@@ -199,10 +209,28 @@ CREATE TABLE `ta_penjualan` (
   `no_penjualan` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `pelanggan_id` tinyint(4) NOT NULL,
   `grand_total` double NOT NULL,
-  `tgl_penjualan` date NOT NULL,
-  `tgl_jatuh_tempo` date NOT NULL,
-  `status_pembayaran` tinyint(1) NOT NULL,
-  `user_id` tinyint(4) NOT NULL,
+  `tgl_penjualan` date DEFAULT NULL,
+  `tgl_jatuh_tempo` date DEFAULT NULL,
+  `status_pembayaran` tinyint(1) DEFAULT NULL,
+  `user_id` tinyint(4) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ta_pulsa`
+--
+
+CREATE TABLE `ta_pulsa` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `kategori` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `operator` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nominal` int(11) NOT NULL,
+  `no_hp` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `harga` int(11) NOT NULL,
+  `user_id` smallint(4) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -219,6 +247,7 @@ CREATE TABLE `users` (
   `username` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
@@ -228,8 +257,8 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `username`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Arifin Aritonang', 'admin', 'admin@gmail.com', '$2y$10$szQi4q1/t1Q2OS8q6jxk6.8FNmbJT0qC4DUWNbp0Lubq2dlYi6RTO', '1CQOCJWh399nDXiWt9zx71mjgHe90JF9viK5hVnvKbHgahQS6eEAmj9BrgZB', '2019-10-16 14:49:11', '2019-10-16 14:49:11');
+INSERT INTO `users` (`id`, `name`, `username`, `email`, `password`, `role`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Admin', 'admin', 'admin@gmail.com', '$2y$10$xrP7Oy9/tSeVytiGqtrD9ObhT5NYCxsm3lZ57anmuAwKgy6D9v0Hy', 'admin', 'JCSKq6DOLWW5H8MnDqWs7mztLbh17y9n36pIvYx2yCeqbKivNCow3xtERim1', '2020-02-17 13:55:07', '2020-02-17 13:55:07');
 
 --
 -- Indexes for dumped tables
@@ -260,6 +289,12 @@ ALTER TABLE `ref_kategori`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `ref_operator_pulsa`
+--
+ALTER TABLE `ref_operator_pulsa`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `ref_pelanggan`
 --
 ALTER TABLE `ref_pelanggan`
@@ -284,6 +319,12 @@ ALTER TABLE `ta_history_barang`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `ta_jasa`
+--
+ALTER TABLE `ta_jasa`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `ta_pembelian`
 --
 ALTER TABLE `ta_pembelian`
@@ -293,6 +334,12 @@ ALTER TABLE `ta_pembelian`
 -- Indexes for table `ta_penjualan`
 --
 ALTER TABLE `ta_penjualan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ta_pulsa`
+--
+ALTER TABLE `ta_pulsa`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -309,7 +356,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `ref_barang`
@@ -327,7 +374,13 @@ ALTER TABLE `ref_distributor`
 -- AUTO_INCREMENT for table `ref_kategori`
 --
 ALTER TABLE `ref_kategori`
-  MODIFY `id` smallint(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` smallint(4) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `ref_operator_pulsa`
+--
+ALTER TABLE `ref_operator_pulsa`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `ref_pelanggan`
@@ -354,16 +407,28 @@ ALTER TABLE `ta_history_barang`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `ta_jasa`
+--
+ALTER TABLE `ta_jasa`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `ta_penjualan`
 --
 ALTER TABLE `ta_penjualan`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `ta_pulsa`
+--
+ALTER TABLE `ta_pulsa`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
